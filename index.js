d607f1ab5a8828ba4139d46c9efdae5302d5e3e8
Kyle Bellingar <kbellingar@gmail.com>
2018-10-20 12:34:30 -0500

    Make stuff and ignore some

---
+++ index.js
@@ -0,0 +1,11 @@
+// Install package via npm install
+// Load Package with require
+var _ = require('lodash');
+
+
+// use package based on variable we import with
+
+for(var i = 0; i<11; i++){
+var r = _.random(3,10);
+console.log(r);
+}
